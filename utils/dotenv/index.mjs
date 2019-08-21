4902f6b1af794442019bbb013c7873a557c71779
Thomas Dillard <thomas.dillard.studios@gmail.com>
2019-08-21 13:22:05 -0700

    updated dotenv to work preload .env variables.

--- utils/dotenv/index.mjs
+++ utils/dotenv/index.mjs
@@ -1,5 +1,21 @@
-function dotEnv() {
-  console.log('test');
+import fs from 'fs';
+import path from 'path';
+import readline from 'readline';
+
+async function dotenv(__dirname) {
+  const envPath = path.resolve(__dirname, '.env');
+  if (fs.existsSync(envPath)) {
+    const rl = readline.createInterface({
+      input: fs.createReadStream(envPath, { encoding: 'utf8' }),
+      console: false
+    });
+
+    for await (const line of rl) {
+      const [key, variable] = line.replace(/"/g, '').split('=');
+      process.env[key] = variable;
+    }
+  }
 }
 
-export default dotEnv;
+export default dotenv;
+
